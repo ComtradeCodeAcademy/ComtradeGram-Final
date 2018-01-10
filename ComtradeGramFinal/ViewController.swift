525fbcf79c706972ff75bbd4f9db202d7b546ddd
pedja <designbyheart@gmail.com>
2018-01-10 18:47:51 +0100

    project intialized

---
+++ ComtradeGramFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComtradeGramFinal
+//
+//  Created by Predrag Jevtic on 1/10/18.
+//  Copyright © 2018 com.comtrade.Gram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
